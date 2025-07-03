384693ed9032de6811e2bb0b43f5a2a2ee9bf19e
Maicon Silva <maiconrs95@gmail.com>
2025-07-03 20:53:17 -0300

    Init Category Entity

---
+++ codeflix/FC3-admin-catalogo-de-videos-typescript/src/domain/category.entity.ts
@@ -0,0 +1,86 @@
+export type CategoryConstructorProps = {
+    category_id?: string;
+    name: string;
+    description?: string | null;
+    is_active?: boolean;
+    created_at?: Date;
+}
+
+export type CategoryCreateCommand = {
+    name: string;
+    description?: string;
+    is_active?: boolean;
+}
+
+/**
+ * Category entity
+ * @description This entity represents a category of a product
+ */
+export class CategoryEntity {
+    category_id: string;
+    name: string;
+    description: string | null;
+    is_active: boolean;
+    created_at: Date;
+
+    constructor(props: CategoryConstructorProps) {
+        this.category_id = props.category_id ?? ''; // update to uuidv4
+        this.name = props.name;
+        this.description = props.description ?? null;
+        this.is_active = props.is_active ?? true;
+        this.created_at = props.created_at ?? new Date();
+    }
+
+    /**
+     * Create category @Factory
+     * @param props - The properties of the category
+     * @returns A new category entity
+     */
+    static create(props: CategoryCreateCommand): CategoryEntity {
+        return new CategoryEntity(props);
+    }
+
+    /**
+     * Change the name of the category
+     * @param name - The new name of the category
+     */
+    changeName(name: string): void {
+        this.name = name;
+    }
+
+    /**
+     * Change the description of the category
+     * @param description - The new description of the category
+     */
+    changeDescription(description: string): void {
+        this.description = description;
+    }
+
+    /**
+     * Activate the category
+     */
+    activate(): void {
+        this.is_active = true;
+    }
+
+    /**
+     * Deactivate the category
+     */
+    deactivate(): void {
+        this.is_active = false;
+    }
+
+    /**
+     * Convert the category to a JSON object
+     * @returns A JSON object representing the category
+     */
+    toJSON(): CategoryConstructorProps {
+        return {
+            category_id: this.category_id,
+            name: this.name,
+            description: this.description,
+            is_active: this.is_active,
+            created_at: this.created_at,
+        };
+    }
+}
