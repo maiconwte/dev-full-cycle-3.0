93cfb039c9a264e2e2a4e2f79709d81fa47b1f23
Maicon Silva <maiconrs95@gmail.com>
2025-07-03 22:43:11 -0300

    feat: add value object pattern and uuid value object

--- codeflix/FC3-admin-catalogo-de-videos-typescript/src/category/domain/__tests__/category.entity.spec.ts
+++ codeflix/FC3-admin-catalogo-de-videos-typescript/src/category/domain/__tests__/category.entity.spec.ts
@@ -1,94 +1,94 @@
 import { CategoryEntity } from "../category.entity";
 
 describe("CategoryEntity Unit Tests", () => {
-    it("should change name", () => {
-        const category = CategoryEntity.create({
-            name: "Movie",
-        });
-        category.changeName("Movie 2");
-        expect(category.name).toBe("Movie 2");
+  it("should change name", () => {
+    const category = CategoryEntity.create({
+      name: "Movie",
+    });
+    category.changeName("Movie 2");
+    expect(category.name).toBe("Movie 2");
+  });
+
+  it("should change description", () => {
+    const category = CategoryEntity.create({
+      name: "Movie",
+    });
+    category.changeDescription("Movie description 2");
+    expect(category.description).toBe("Movie description 2");
+  });
+
+  it("should change active", () => {
+    const category = CategoryEntity.create({
+      name: "Movie",
+      is_active: false,
+    });
+    category.activate();
+    expect(category.is_active).toBe(true);
+  });
+
+  it("should change deactive", () => {
+    const category = CategoryEntity.create({
+      name: "Movie",
+      is_active: true,
+    });
+    category.deactivate();
+    expect(category.is_active).toBe(false);
+  });
+
+  describe("constructor", () => {
+    it("should be able to create a category with default values", () => {
+      // DDD: Arrange Act Assert
+      let category = new CategoryEntity({
+        name: "Movie",
+      });
+      expect(category.category_id).toBeDefined();
+      expect(category.name).toBe("Movie");
+      expect(category.description).toBeNull();
+      expect(category.is_active).toBe(true);
+      expect(category.created_at).toBeInstanceOf(Date);
     });
 
-    it("should change description", () => {
-        const category = CategoryEntity.create({
-            name: "Movie",
-        });
-        category.changeDescription("Movie description 2");
-        expect(category.description).toBe("Movie description 2");
+    it("should be able to create a category with all values", () => {
+      const created_at = new Date();
+      const category = new CategoryEntity({
+        name: "Movie",
+        description: "Movie description",
+        is_active: false,
+        created_at: created_at,
+      });
+
+      expect(category.category_id).toBeDefined();
+      expect(category.name).toBe("Movie");
+      expect(category.description).toBe("Movie description");
+      expect(category.is_active).toBe(false);
+      expect(category.created_at).toBe(created_at);
     });
 
-    it("should change active", () => {
-        const category = CategoryEntity.create({
-            name: "Movie",
-            is_active: false,
-        });
-        category.activate();
-        expect(category.is_active).toBe(true);
+    it("should be able to create a category with partial values", () => {
+      const category = new CategoryEntity({
+        name: "Movie",
+        description: null,
+      });
+
+      expect(category.category_id).toBeDefined();
+      expect(category.name).toBe("Movie");
+      expect(category.description).toBeNull();
+      expect(category.is_active).toBe(true);
+      expect(category.created_at).toBeInstanceOf(Date);
     });
+  });
 
-    it("should change deactive", () => {
-        const category = CategoryEntity.create({
-            name: "Movie",
-            is_active: true,
-        });
-        category.deactivate();
-        expect(category.is_active).toBe(false);
+  describe("create command", () => {
+    it("should be able to create a category", () => {
+      const category = CategoryEntity.create({
+        name: "Movie",
+      });
+
+      expect(category.category_id).toBeDefined();
+      expect(category.name).toBe("Movie");
+      expect(category.description).toBeNull();
+      expect(category.is_active).toBe(true);
+      expect(category.created_at).toBeInstanceOf(Date);
     });
-
-    describe("constructor", () => {
-        it("should be able to create a category with default values", () => {
-            // DDD: Arrange Act Assert
-            let category = new CategoryEntity({
-                name: "Movie",
-            });
-            expect(category.category_id).toBeDefined();
-            expect(category.name).toBe("Movie");
-            expect(category.description).toBeNull();
-            expect(category.is_active).toBe(true);
-            expect(category.created_at).toBeInstanceOf(Date);
-        });
-
-        it("should be able to create a category with all values", () => {
-            const created_at = new Date();
-            const category = new CategoryEntity({
-                name: "Movie",
-                description: "Movie description",
-                is_active: false,
-                created_at: created_at,
-            });
-
-            expect(category.category_id).toBeDefined();
-            expect(category.name).toBe("Movie");
-            expect(category.description).toBe("Movie description");
-            expect(category.is_active).toBe(false);
-            expect(category.created_at).toBe(created_at);
-        });
-
-        it("should be able to create a category with partial values", () => {
-            const category = new CategoryEntity({
-                name: "Movie",
-                description: null,
-            });
-
-            expect(category.category_id).toBeDefined();
-            expect(category.name).toBe("Movie");
-            expect(category.description).toBeNull();
-            expect(category.is_active).toBe(true);
-            expect(category.created_at).toBeInstanceOf(Date);
-        });
-    });
-
-    describe("create command", () => {
-        it("should be able to create a category", () => {
-            const category = CategoryEntity.create({
-                name: "Movie",
-            });
-
-            expect(category.category_id).toBeDefined();
-            expect(category.name).toBe("Movie");
-            expect(category.description).toBeNull();
-            expect(category.is_active).toBe(true);
-            expect(category.created_at).toBeInstanceOf(Date);
-        });
-    });
+  });
 });
--- codeflix/FC3-admin-catalogo-de-videos-typescript/src/category/domain/category.entity.ts
+++ codeflix/FC3-admin-catalogo-de-videos-typescript/src/category/domain/category.entity.ts
@@ -1,15 +1,15 @@
 export type CategoryConstructorProps = {
-    category_id?: string;
-    name: string;
-    description?: string | null;
-    is_active?: boolean;
-    created_at?: Date;
+  category_id?: string;
+  name: string;
+  description?: string | null;
+  is_active?: boolean;
+  created_at?: Date;
 }
 
 export type CategoryCreateCommand = {
-    name: string;
-    description?: string;
-    is_active?: boolean;
+  name: string;
+  description?: string;
+  is_active?: boolean;
 }
 
 /**
@@ -17,70 +17,70 @@ export type CategoryCreateCommand = {
  * @description This entity represents a category of a product
  */
 export class CategoryEntity {
-    category_id: string;
-    name: string;
-    description: string | null;
-    is_active: boolean;
-    created_at: Date;
+  category_id: string;
+  name: string;
+  description: string | null;
+  is_active: boolean;
+  created_at: Date;
 
-    constructor(props: CategoryConstructorProps) {
-        this.category_id = props.category_id ?? ''; // update to uuidv4
-        this.name = props.name;
-        this.description = props.description ?? null;
-        this.is_active = props.is_active ?? true;
-        this.created_at = props.created_at ?? new Date();
-    }
+  constructor(props: CategoryConstructorProps) {
+    this.category_id = props.category_id ?? ''; // update to uuidv4
+    this.name = props.name;
+    this.description = props.description ?? null;
+    this.is_active = props.is_active ?? true;
+    this.created_at = props.created_at ?? new Date();
+  }
 
-    /**
-     * Create category @Factory
-     * @param props - The properties of the category
-     * @returns A new category entity
-     */
-    static create(props: CategoryCreateCommand): CategoryEntity {
-        return new CategoryEntity(props);
-    }
+  /**
+   * Create category @Factory
+   * @param props - The properties of the category
+   * @returns A new category entity
+   */
+  static create(props: CategoryCreateCommand): CategoryEntity {
+    return new CategoryEntity(props);
+  }
 
-    /**
-     * Change the name of the category
-     * @param name - The new name of the category
-     */
-    changeName(name: string): void {
-        this.name = name;
-    }
+  /**
+   * Change the name of the category
+   * @param name - The new name of the category
+   */
+  changeName(name: string): void {
+    this.name = name;
+  }
 
-    /**
-     * Change the description of the category
-     * @param description - The new description of the category
-     */
-    changeDescription(description: string): void {
-        this.description = description;
-    }
+  /**
+   * Change the description of the category
+   * @param description - The new description of the category
+   */
+  changeDescription(description: string): void {
+    this.description = description;
+  }
 
-    /**
-     * Activate the category
-     */
-    activate(): void {
-        this.is_active = true;
-    }
+  /**
+   * Activate the category
+   */
+  activate(): void {
+    this.is_active = true;
+  }
 
-    /**
-     * Deactivate the category
-     */
-    deactivate(): void {
-        this.is_active = false;
-    }
+  /**
+   * Deactivate the category
+   */
+  deactivate(): void {
+    this.is_active = false;
+  }
 
-    /**
-     * Convert the category to a JSON object
-     * @returns A JSON object representing the category
-     */
-    toJSON(): CategoryConstructorProps {
-        return {
-            category_id: this.category_id,
-            name: this.name,
-            description: this.description,
-            is_active: this.is_active,
-            created_at: this.created_at,
-        };
-    }
+  /**
+   * Convert the category to a JSON object
+   * @returns A JSON object representing the category
+   */
+  toJSON(): CategoryConstructorProps {
+    return {
+      category_id: this.category_id,
+      name: this.name,
+      description: this.description,
+      is_active: this.is_active,
+      created_at: this.created_at,
+    };
+  }
 }
