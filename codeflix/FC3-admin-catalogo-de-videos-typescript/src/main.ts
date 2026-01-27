388ad442fd98205dff1a5af4d113c83cfac0208a
Maicon Silva <maiconrs95@gmail.com>
2026-01-27 00:22:44 -0300

    feature: Implement Get Categories with Categories presenter

--- codeflix/FC3-admin-catalogo-de-videos-typescript/src/main.ts
+++ codeflix/FC3-admin-catalogo-de-videos-typescript/src/main.ts
@@ -1,8 +1,24 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {
+  ClassSerializerInterceptor,
+  HttpStatus,
+  ValidationPipe,
+} from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create(AppModule, {
+    logger: process.env.NODE_ENV === 'production' ? console : undefined,
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY,
+    }),
+  );
+
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+
+  await app.listen(3000);
 }
 bootstrap();
--- codeflix/FC3-admin-catalogo-de-videos-typescript/src/nest-modules/categories-module/categories.controller.ts
+++ codeflix/FC3-admin-catalogo-de-videos-typescript/src/nest-modules/categories-module/categories.controller.ts
@@ -1,37 +1,92 @@
 import {
   Controller,
-  Get,
+  // Get,
   Post,
   Body,
-  Patch,
-  Param,
-  Delete,
+  // Patch,
+  // Param,
+  // Delete,
+  Inject,
+  // ParseUUIDPipe,
+  // HttpCode,
+  // Query,
+  // // UseGuards,
 } from '@nestjs/common';
 import { CreateCategoryDto } from './dto/create-category.dto';
-import { UpdateCategoryDto } from './dto/update-category.dto';
-import { CategorySequelizeRepository } from '@core/category/infra/db/sequelize/category-sequelize.repository';
+// import { UpdateCategoryDto } from './dto/update-category.dto';
+import { CreateCategoryUseCase } from '../../core/category/application/use-cases/create-category/create-category.use-case';
+// import { UpdateCategoryUseCase } from '../../core/category/application/use-cases/update-category/update-category.use-case';
+// import { DeleteCategoryUseCase } from '../../core/category/application/use-cases/delete-category/delete-category.use-case';
+// import { GetCategoryUseCase } from '../../core/category/application/use-cases/get-category/get-category.use-case';
+// import { ListCategoriesUseCase } from '../../core/category/application/use-cases/list-categories/list-categories.use-case';
+import {
+  // CategoryCollectionPresenter,
+  CategoryPresenter,
+} from './categories.presenter';
+import { CategoryOutput } from '../../core/category/application/use-cases/common/category-output';
+// import { SearchCategoriesDto } from './dto/search-categories.dto';
+// import { AuthGuard } from '../auth-module/auth.guard';
+// import { CheckIsAdminGuard } from '../auth-module/check-is-admin.guard';
 
+// @UseGuards(AuthGuard, CheckIsAdminGuard)
 @Controller('categories')
 export class CategoriesController {
-  constructor(private categoryRepository: CategorySequelizeRepository) {
-    console.log('categoryRepository', this.categoryRepository);
-  }
+  @Inject(CreateCategoryUseCase)
+  private createUseCase: CreateCategoryUseCase;
+
+  // @Inject(UpdateCategoryUseCase)
+  // private updateUseCase: UpdateCategoryUseCase;
+
+  // @Inject(DeleteCategoryUseCase)
+  // private deleteUseCase: DeleteCategoryUseCase;
+
+  // @Inject(GetCategoryUseCase)
+  // private getUseCase: GetCategoryUseCase;
+
+  // @Inject(ListCategoriesUseCase)
+  // private listUseCase: ListCategoriesUseCase;
 
   @Post()
-  create(@Body() createCategoryDto: CreateCategoryDto) {}
+  async create(@Body() createCategoryDto: CreateCategoryDto) {
+    const output = await this.createUseCase.execute(createCategoryDto);
+    return CategoriesController.serialize(output);
+  }
 
-  @Get()
-  findAll() {}
+  // @Get()
+  // async search(@Query() searchParamsDto: SearchCategoriesDto) {
+  //   const output = await this.listUseCase.execute(searchParamsDto);
+  //   return new CategoryCollectionPresenter(output);
+  // }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {}
+  // @Get(':id')
+  // async findOne(
+  //   @Param('id', new ParseUUIDPipe({ errorHttpStatusCode: 422 })) id: string,
+  // ) {
+  //   const output = await this.getUseCase.execute({ id });
+  //   return CategoriesController.serialize(output);
+  // }
 
-  @Patch(':id')
-  update(
-    @Param('id') id: string,
-    @Body() updateCategoryDto: UpdateCategoryDto,
-  ) {}
+  // @Patch(':id')
+  // async update(
+  //   @Param('id', new ParseUUIDPipe({ errorHttpStatusCode: 422 })) id: string,
+  //   @Body() updateCategoryDto: UpdateCategoryDto,
+  // ) {
+  //   const output = await this.updateUseCase.execute({
+  //     ...updateCategoryDto,
+  //     id,
+  //   });
+  //   return CategoriesController.serialize(output);
+  // }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {}
+  // @HttpCode(204)
+  // @Delete(':id')
+  // remove(
+  //   @Param('id', new ParseUUIDPipe({ errorHttpStatusCode: 422 })) id: string,
+  // ) {
+  //   return this.deleteUseCase.execute({ id });
+  // }
+
+  static serialize(output: CategoryOutput) {
+    return new CategoryPresenter(output);
+  }
 }
