cdaf8e684c69fc67fc585f096c940c943921137e
Maicon Silva <maiconrs95@gmail.com>
2026-01-27 00:31:58 -0300

    feature: Implement Search Categories with Categories presenter

--- codeflix/FC3-admin-catalogo-de-videos-typescript/src/nest-modules/categories-module/categories.controller.ts
+++ codeflix/FC3-admin-catalogo-de-videos-typescript/src/nest-modules/categories-module/categories.controller.ts
@@ -1,6 +1,6 @@
 import {
   Controller,
-  // Get,
+  Get,
   Post,
   Body,
   Patch,
@@ -9,22 +9,22 @@ import {
   Inject,
   ParseUUIDPipe,
   HttpCode,
-  // Query,
-  // // UseGuards,
+  Query,
+  // UseGuards,
 } from '@nestjs/common';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
 import { CreateCategoryUseCase } from '../../core/category/application/use-cases/create-category/create-category.use-case';
 import { UpdateCategoryUseCase } from '../../core/category/application/use-cases/update-category/update-category.use-case';
 import { DeleteCategoryUseCase } from '../../core/category/application/use-cases/delete-category/delete-category.use-case';
-// import { GetCategoryUseCase } from '../../core/category/application/use-cases/get-category/get-category.use-case';
-// import { ListCategoriesUseCase } from '../../core/category/application/use-cases/list-categories/list-categories.use-case';
+import { GetCategoryUseCase } from '../../core/category/application/use-cases/get-category/get-category.use-case';
+import { ListCategoriesUseCase } from '../../core/category/application/use-cases/list-categories/list-categories.use-case';
 import {
-  // CategoryCollectionPresenter,
+  CategoryCollectionPresenter,
   CategoryPresenter,
 } from './categories.presenter';
 import { CategoryOutput } from '../../core/category/application/use-cases/common/category-output';
-// import { SearchCategoriesDto } from './dto/search-categories.dto';
+import { SearchCategoriesDto } from './dto/search-categories.dto';
 // import { AuthGuard } from '../auth-module/auth.guard';
 // import { CheckIsAdminGuard } from '../auth-module/check-is-admin.guard';
 
@@ -40,11 +40,11 @@ export class CategoriesController {
   @Inject(DeleteCategoryUseCase)
   private deleteUseCase: DeleteCategoryUseCase;
 
-  // @Inject(GetCategoryUseCase)
-  // private getUseCase: GetCategoryUseCase;
+  @Inject(GetCategoryUseCase)
+  private getUseCase: GetCategoryUseCase;
 
-  // @Inject(ListCategoriesUseCase)
-  // private listUseCase: ListCategoriesUseCase;
+  @Inject(ListCategoriesUseCase)
+  private listUseCase: ListCategoriesUseCase;
 
   @Post()
   async create(@Body() createCategoryDto: CreateCategoryDto) {
@@ -52,19 +52,19 @@ export class CategoriesController {
     return CategoriesController.serialize(output);
   }
 
-  // @Get()
-  // async search(@Query() searchParamsDto: SearchCategoriesDto) {
-  //   const output = await this.listUseCase.execute(searchParamsDto);
-  //   return new CategoryCollectionPresenter(output);
-  // }
+  @Get()
+  async search(@Query() searchParamsDto: SearchCategoriesDto) {
+    const output = await this.listUseCase.execute(searchParamsDto);
+    return new CategoryCollectionPresenter(output);
+  }
 
-  // @Get(':id')
-  // async findOne(
-  //   @Param('id', new ParseUUIDPipe({ errorHttpStatusCode: 422 })) id: string,
-  // ) {
-  //   const output = await this.getUseCase.execute({ id });
-  //   return CategoriesController.serialize(output);
-  // }
+  @Get(':id')
+  async findOne(
+    @Param('id', new ParseUUIDPipe({ errorHttpStatusCode: 422 })) id: string,
+  ) {
+    const output = await this.getUseCase.execute({ id });
+    return CategoriesController.serialize(output);
+  }
 
   @Patch(':id')
   async update(
