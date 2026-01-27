ae4176b4f40abf4ed4a30fd7fca5a471497b8d9c
Maicon Silva <maiconrs95@gmail.com>
2026-01-27 00:27:06 -0300

    feature: Implement Patch Categories with Categories presenter

--- codeflix/FC3-admin-catalogo-de-videos-typescript/src/nest-modules/categories-module/categories.controller.ts
+++ codeflix/FC3-admin-catalogo-de-videos-typescript/src/nest-modules/categories-module/categories.controller.ts
@@ -3,19 +3,19 @@ import {
   // Get,
   Post,
   Body,
-  // Patch,
-  // Param,
+  Patch,
+  Param,
   // Delete,
   Inject,
-  // ParseUUIDPipe,
+  ParseUUIDPipe,
   // HttpCode,
   // Query,
   // // UseGuards,
 } from '@nestjs/common';
 import { CreateCategoryDto } from './dto/create-category.dto';
-// import { UpdateCategoryDto } from './dto/update-category.dto';
+import { UpdateCategoryDto } from './dto/update-category.dto';
 import { CreateCategoryUseCase } from '../../core/category/application/use-cases/create-category/create-category.use-case';
-// import { UpdateCategoryUseCase } from '../../core/category/application/use-cases/update-category/update-category.use-case';
+import { UpdateCategoryUseCase } from '../../core/category/application/use-cases/update-category/update-category.use-case';
 // import { DeleteCategoryUseCase } from '../../core/category/application/use-cases/delete-category/delete-category.use-case';
 // import { GetCategoryUseCase } from '../../core/category/application/use-cases/get-category/get-category.use-case';
 // import { ListCategoriesUseCase } from '../../core/category/application/use-cases/list-categories/list-categories.use-case';
@@ -34,8 +34,8 @@ export class CategoriesController {
   @Inject(CreateCategoryUseCase)
   private createUseCase: CreateCategoryUseCase;
 
-  // @Inject(UpdateCategoryUseCase)
-  // private updateUseCase: UpdateCategoryUseCase;
+  @Inject(UpdateCategoryUseCase)
+  private updateUseCase: UpdateCategoryUseCase;
 
   // @Inject(DeleteCategoryUseCase)
   // private deleteUseCase: DeleteCategoryUseCase;
@@ -66,17 +66,17 @@ export class CategoriesController {
   //   return CategoriesController.serialize(output);
   // }
 
-  // @Patch(':id')
-  // async update(
-  //   @Param('id', new ParseUUIDPipe({ errorHttpStatusCode: 422 })) id: string,
-  //   @Body() updateCategoryDto: UpdateCategoryDto,
-  // ) {
-  //   const output = await this.updateUseCase.execute({
-  //     ...updateCategoryDto,
-  //     id,
-  //   });
-  //   return CategoriesController.serialize(output);
-  // }
+  @Patch(':id')
+  async update(
+    @Param('id', new ParseUUIDPipe({ errorHttpStatusCode: 422 })) id: string,
+    @Body() updateCategoryDto: UpdateCategoryDto,
+  ) {
+    const output = await this.updateUseCase.execute({
+      ...updateCategoryDto,
+      id,
+    });
+    return CategoriesController.serialize(output);
+  }
 
   // @HttpCode(204)
   // @Delete(':id')
