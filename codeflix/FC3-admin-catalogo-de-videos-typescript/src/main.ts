b64c584fb828c15e01b6472ae7052e59f23b8eb9
Maicon Silva <maiconrs95@gmail.com>
2026-01-29 00:20:43 -0300

    feature: Added not found and entity validation nest global error handler

--- codeflix/FC3-admin-catalogo-de-videos-typescript/src/main.ts
+++ codeflix/FC3-admin-catalogo-de-videos-typescript/src/main.ts
@@ -6,6 +6,8 @@ import {
   ValidationPipe,
 } from '@nestjs/common';
 import { WrapperDataInterceptor } from './nest-modules/shared-module/interceptors/wrapper-data/wrapper-data.interceptor';
+import { NotFoundErrorFilter } from './nest-modules/shared-module/filters/not-found/not-found.filter';
+import { EntityValidationErrorFilter } from './nest-modules/shared-module/filters/not-found/entity-validation-error.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -20,6 +22,10 @@ async function bootstrap() {
 
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
   app.useGlobalInterceptors(new WrapperDataInterceptor());
+  app.useGlobalFilters(
+    new NotFoundErrorFilter(),
+    new EntityValidationErrorFilter(),
+  );
 
   await app.listen(3000);
 }
