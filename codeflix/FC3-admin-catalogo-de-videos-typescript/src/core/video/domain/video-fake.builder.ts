30b4a0c7053540aacf0179f50780d2811a46009c
Maicon Silva <maiconrs95@gmail.com>
2026-04-14 22:30:41 -0300

    feature: add missed dependencies

--- codeflix/FC3-admin-catalogo-de-videos-typescript/src/core/video/domain/video-fake.builder.ts
+++ codeflix/FC3-admin-catalogo-de-videos-typescript/src/core/video/domain/video-fake.builder.ts
@@ -270,7 +270,7 @@ export class VideoFakeBuilder<TBuild = any> {
       video.validate();
       return video;
     });
-    return this.countObjs === 1 ? (videos[0] as any) : videos;
+    return (this.countObjs === 1 ? videos[0] : videos) as TBuild;
   }
 
   get video_id() {
