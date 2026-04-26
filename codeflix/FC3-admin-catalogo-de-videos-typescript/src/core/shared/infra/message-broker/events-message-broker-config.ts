41e585f3c169251938a4d66f551e62c73e175a0e
Maicon Silva <maiconrs95@gmail.com>
2026-04-25 21:06:12 -0300

    feature: add rabbitimq module

---
+++ codeflix/FC3-admin-catalogo-de-videos-typescript/src/core/shared/infra/message-broker/events-message-broker-config.ts
@@ -0,0 +1,15 @@
+import { VideoAudioMediaUploadedIntegrationEvent } from '../../../video/domain/domain-events/video-audio-media-replaced.event';
+
+// eslint-disable-next-line @typescript-eslint/no-unused-expressions
+VideoAudioMediaUploadedIntegrationEvent;
+
+export const EVENTS_MESSAGE_BROKER_CONFIG = {
+  [VideoAudioMediaUploadedIntegrationEvent.name]: {
+    exchange: 'amq.direct',
+    routing_key: VideoAudioMediaUploadedIntegrationEvent.name,
+  },
+  TestEvent: {
+    exchange: 'test-exchange',
+    routing_key: 'TestEvent',
+  },
+};
